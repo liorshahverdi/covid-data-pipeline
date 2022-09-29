6088bac50471264ee20b15ab1272bda6d9052fac
Lior Shahverdi <liorshahverdi@gmail.com>
2022-09-28 21:50:03 -0400

    testing, cleanup changes to main.py.

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import io, requests
+import os, sys, io, requests
 from datetime import datetime, timedelta
 import pandas as pd
 import psycopg2
+
+sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 def connect():
 	"""
@@ -76,9 +78,16 @@ def write_to_db(result_dict: dict, cur: psycopg2.extensions.cursor) -> None:
 
 		Returns None.
 	"""
-	query = f"INSERT INTO deaths VALUES ('{result_dict['state']}', '{result_dict['county']}', {result_dict['deaths']}, '{result_dict['date']}')"
-	print (query)
-	cur.execute(query)
+	keys = ['state', 'county', 'deaths', 'date']
+	if all([key in result_dict for key in keys]):
+		query = f"INSERT INTO deaths VALUES ('{result_dict['state']}', '{result_dict['county']}', {result_dict['deaths']}, '{result_dict['date']}')"
+		try:
+			cur.execute(query)
+			print (query)
+		except:
+			print (f'There is already a record for {result_dict["state"]} on {result_dict["date"]}. Skipping.')
+	else:
+		raise Exception(f'result_dict is missing keys. result_dict: {result_dict}')
 
 def select_from_db(cur: psycopg2.extensions.cursor) -> None:
 	"""
@@ -91,7 +100,7 @@ def select_from_db(cur: psycopg2.extensions.cursor) -> None:
 		Returns None
 	"""
 	cur.execute("SELECT * FROM deaths")
-	print (cur.fetchone()) 
+	return cur.fetchone()
 
 """
 df = get_live_county_data()
@@ -122,4 +131,5 @@ def load(state_names: list):
 if __name__ == '__main__':
 	extract()
 	state_names = ["Alaska", "Alabama", "Arkansas", "American Samoa", "Arizona", "California", "Colorado", "Connecticut", "District of Columbia", "Delaware", "Florida", "Georgia", "Guam", "Hawaii", "Iowa", "Idaho", "Illinois", "Indiana", "Kansas", "Kentucky", "Louisiana", "Massachusetts", "Maryland", "Maine", "Michigan", "Minnesota", "Missouri", "Mississippi", "Montana", "North Carolina", "North Dakota", "Nebraska", "New Hampshire", "New Jersey", "New Mexico", "Nevada", "New York", "Ohio", "Oklahoma", "Oregon", "Pennsylvania", "Puerto Rico", "Rhode Island", "South Carolina", "South Dakota", "Tennessee", "Texas", "Utah", "Virginia", "Virgin Islands", "Vermont", "Washington", "Wisconsin", "West Virginia", "Wyoming"]
-	load(state_names)	
+	load(state_names)
+	os.remove('daily.csv')
